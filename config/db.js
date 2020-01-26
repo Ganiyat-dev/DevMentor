abfd397e65cf42400a7d52cfcd9c3811b899fc55
calvin <puram.calvin@gmail.com>
2020-01-26 11:46:29 +0100

    setup mongodb atlas with compass and app connecting to it, unhandled rejection & exception

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDb = require('./config/db.js');
+
+// handle uncaught exception
+process.on('uncaughtException', err => {
+  console.log(`uncaught exception: ${err.message}`);
+  process.exit(1);
+});
+
 //routes
 const bootcampsRoutes = require('./routes/bootcamps');
 
 const app = express();
+
 dotenv.config({ path: './config/config.env' });
+
+//connect db
+connectDb();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('tiny'));
@@ -15,8 +27,16 @@ app.use('/api/v1/bootcamps', bootcampsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `server is in ${process.env.NODE_ENV} mode & listening on port ${process.env.PORT}`
   );
+});
+
+//unhandled promise rejection
+process.on('unhandledRejection', err => {
+  console.log(`unhandled rejection: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
