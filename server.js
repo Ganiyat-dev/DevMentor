4c26960a91e8126d1e2fa072b0dbfd866111fa0f
calvin <puram.calvin@gmail.com>
2020-01-26 10:46:38 +0100

    setup morgan and postman desc for bootcamps routes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 //routes
 const bootcampsRoutes = require('./routes/bootcamps');
 
 const app = express();
 dotenv.config({ path: './config/config.env' });
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('tiny'));
+}
 
 app.use('/api/v1/bootcamps', bootcampsRoutes);
 
